6719cfb260f10f952a254cc9f26883b651f4d655
Robert Cai <xiaoyucai2015@gmail.com>
2021-10-17 16:36:19 -0400

    Fix missing bracket

--- example/Ratio.java
+++ example/Ratio.java
@@ -1,6 +1,6 @@
 public class Ratio {
   public static double calculate(double width, double height) {
-    if width > height return width/height;
+    if (width > height) return width/height;
     else return height/width;
   }
 }
